6c85b8841e0c46abe40bf5a3dcae769beab1f60a
MarcinZietek <Administrator@WINSRV2019BD>
2026-01-10 12:15:21 +0100

    Zad1 praca ze zmiennymi

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,42 +1,29 @@
+#Najpierw utwórz zmienn¹ o nazwie x i przypisz jej wartoœæ 10
+x = 10
 
-print("KALKULATOR")
-print("1 - dodawanie")
-print("2 - odejmowanie")
-print("3 - mnozenie")
-print("4 - dzielenie")
-while True:
-    operation = input("Wybierz dzialanie (1-4)")
-    a = float(input("Podaj pierwsza liczbe: "))
-    b = float(input("Podaj druga liczbe: "))
-  
-    verification = input("Czy zatwierdzic? (t/n)")
-    calculation = 0
+#Nastêpnie utwórz drug¹ zmienn¹ y i przypisz jej wartoœæ 55
+y = 55
 
-    if verification == "t":
-        print("Pierwsza liczba: ", a, "\n")
-        print("Druga liczba: ", b, "\n")
-        if operation == "1":
-            calculation = a + b 
-            print("Wynik: ", calculation)
-        elif operation == "2":
-            calculation = a - b
-            print("Wynik: ", calculation)
-        elif operation == "3":
-            calculation = a * b
-            print("Wynik: ", calculation)
-        elif operation == "4":
-            calculation = a / b
-            print("Wynik: ", calculation)
-        else:
-            print("Anulowano!\n")
-       
-    nextData = input("Czy wykonac kolejne dzialanie? (t/n)")
-    if nextData != "t":
-        break
+#Utwórz zmienn¹ z i przypisz jej wartoœæ x + y
+z = x + y
 
+#Zast¹p x wartoœci¹ z
+x = z
 
+#Wydrukuj wartoœæ x
+print(x)
 
+#Rodzaje zmiennych
+a = 10
+b = 3,14
+c = "zmienna"
+d = True
+e = [1,2,3]
+f = (1,2)
+g = {"x":1,"y":2}
+h = None
 
+variables = [a,b,c,d,e,f,g,h]
 
-
-
+for z in variables:
+    print(type(z))
