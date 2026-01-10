c621d4fac03b3a4fa4153663781a55b7cff83fcf
MarcinZietek <Administrator@WINSRV2019BD>
2026-01-10 11:55:52 +0100

    Zad5 kalkulator

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,23 +1,37 @@
 
-
+print("KALKULATOR")
+print("1 - dodawanie")
+print("2 - odejmowanie")
+print("3 - mnozenie")
+print("4 - dzielenie")
 while True:
-    name = input("Podaj imie: ")
-    surname = input("Podaj nazwisko: ")
-    age = input("Podaj wiek: ")
-
-    print("Wprowadzone dane: ",name,surname,age)
-
+    operation = input("Wybierz dzialanie (1-4)")
+    a = float(input("Podaj pierwsza liczbe: "))
+    b = float(input("Podaj druga liczbe: "))
+  
     verification = input("Czy zatwierdzic? (t/n)")
+    calculation = 0
 
     if verification == "t":
-        print("Imie: " + name + "\n")
-        print("Nazwisko: " + surname + "\n")
-        print("Wiek: " + age + "\n")
-    else:
-        print("Anulowano!\n")
-
-    nextData = input("Czy zakonczyc wyswietlanie danych uzytkownika? (t/n)")
-    if nextData != "n":
+        print("Pierwsza liczba: ", a, "\n")
+        print("Druga liczba: ", b, "\n")
+        if operation == "1":
+            calculation = a + b 
+            print("Wynik: ", calculation)
+        elif operation == "2":
+            calculation = a - b
+            print("Wynik: ", calculation)
+        elif operation == "3":
+            calculation = a * b
+            print("Wynik: ", calculation)
+        elif operation == "4":
+            calculation = a / b
+            print("Wynik: ", calculation)
+        else:
+            print("Anulowano!\n")
+       
+    nextData = input("Czy wykonac kolejne dzialanie? (t/n)")
+    if nextData != "t":
         break
 
 
