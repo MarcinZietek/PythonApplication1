7bb08b174ff5897f565cf3316df785042d6acd33
MarcinZietek <Administrator@WINSRV2019BD>
2026-01-10 15:02:45 +0100

    Zad4 ciekawskie dziecko

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,16 +1,8 @@
-text = input ("Podaj tekst: ")
+print("Dlacego ludzie maja pecha?\n") 
 
-vowel = "aeiouyAEIOUY"
-vowelCounter = 0
-consonantCounter = 0
-
-for letter in text:
-    if letter.isalpha():
-        if letter in vowel:
-            vowelCounter += 1
-        else:
-            consonantCounter += 1
-
-print("Lczba samoglosek: ", vowelCounter,"\n")
-print("Lczba spolglosek: ", consonantCounter,"\n")
-
+while True:
+    answer = input("Odpowiedz: ")
+    if answer != ("Dosyc"):
+        print("Dalcego?")
+    else:
+        break
